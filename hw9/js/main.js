48aa5ea66810af86a9a979801d9d395042259349
Kirill Bogdanov <kupuk.87@gmail.com>
2020-06-21 15:33:53 +0300

    HW9 - done

--- hw9/js/main.js
+++ hw9/js/main.js
@@ -51,7 +51,6 @@ document.querySelector('.field_todo').addEventListener('keydown', function (e) {
 });
 
 document.querySelector('.clean').addEventListener('click', function () {
-
     let field = document.querySelector('.todo');
     field.innerHTML = "";
     localStorage.removeItem('todo', field.innerHTML);
