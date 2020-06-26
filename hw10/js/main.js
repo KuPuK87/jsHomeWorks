6a3cd296e2f87d0d200d2d4fe2d35507c0963f33
Kirill Bogdanov <kupuk.87@gmail.com>
2020-06-26 21:49:56 +0300

    hw10 - correct

--- hw10/js/main.js
+++ hw10/js/main.js
@@ -1,3 +1,5 @@
+// Это объект из седьмого задания
+
 let Lightbulb = function (name) {
 
     this.get = function () {
@@ -34,7 +36,7 @@ let Lightbulb = function (name) {
             minuts = Math.round(60 - ((differentRes % 86400000) % 3600000) / 60000);
         }
 
-        this.totalTime = hours + (minuts / 60)
+        this.totalTime = hours + (minuts / 60);
 
         this.result = (this.power / 1000) * this.costEnergy * this.totalTime;
 
@@ -52,9 +54,8 @@ let Lightbulb = function (name) {
 };
 
 let lightBulb = new Lightbulb('Светильник - Кухня');
-// lightBulb.get();
 
-// Это 7е дз
+// lightBulb.get();
 
 // Задание: Используя полученные знания, создайте новый объект, который будет наследовать
 // свойства и методы объекта, созданного из предыдущего домашнего задания. Выполните
@@ -78,21 +79,53 @@ let Conditioning = function (power, status) {
             console.log('Кондиционер не включен');
             return
         };
-      
-        if (this.firstDate == '' || this.secondDate == '') {
+
+        let RegExp = /\d{2}:\d{2}/;
+
+        if (!(RegExp.test(this.firstDate)) || !(RegExp.test(this.secondDate))) {
             console.log('Кондиционер не включен');
             return
         }
-        this.totalPower();
+
+        this.mode();
     };
+
+    this.mode = function () {
+        
+        this.selectMode = +prompt('Введите режим работы - 1, 2, 3, 4, 5');
+
+        switch (this.selectMode) {
+            case 1:
+                this.powerMode = 0.2;
+                break;
+            case 2:
+                this.powerMode = 0.4;
+                break;
+            case 3:
+                this.powerMode = 0.6;
+                break;
+            case 4:
+                this.powerMode = 0.8;
+                break;
+            case 5:
+                this.powerMode = 1;
+                break;
+            default:
+                alert('Такого режима не существует, кондиционер включен на минимальный режим');
+                this.powerMode = 0.2;
+        }
+
+        this.totalPower();
+
+    }
 
     this.totalPower = function () {
 
         if (confirm('Включить кондиционер на обогрев?')) {
-            this.power = this.power;
+            this.power = this.power * this.powerMode;
             console.log('Кондиционер включен на обогрев')
         } else {
-            this.power = 0.7 * this.power;
+            this.power = 0.7 * this.power * this.powerMode;
             console.log('Кондиционер включен на охлаждение')
         };
 
